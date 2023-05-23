db6ecd4ea0db10ca03b73ece1713a2b8e640c9de
Stefano Fancello <stefano.fancello@nethesis.it>
2023-05-23 11:38:38 +0200

    Speccify columns so module can be correctly installed even if some optional column are added like for FPBX16

--- install.php
+++ install.php
@@ -5,11 +5,11 @@ global $db;
 global $amp_conf;
 
 $sqls = array();
-$sqls[] = "INSERT IGNORE INTO `featurecodes` VALUES ('extraoptions','transferdigittimeout','Seconds to wait between digits when transferring','In-Call. Number of seconds to wait between digits when transferring a call','3',NULL,1,0)";
-$sqls[] = "INSERT IGNORE INTO `featurecodes` VALUES ('extraoptions','featuredigittimeout','Max time between digits for feature activation','In-Call. Max time (ms) between digits for feature activation (default is 1000 ms)','1000',NULL,1,0)";
-$sqls[] = "INSERT IGNORE INTO `featurecodes` VALUES ('extraoptions','atxfernoanswertimeout','Attended transfer timeout','In-Call. Timeout for answer on attended transfer default is 15 seconds.','15',NULL,1,0)";
-$sqls[] = "INSERT IGNORE INTO `featurecodes` VALUES ('extraoptions','atxferloopdelay','Number of seconds to sleep between retries','In-Call. Number of seconds to sleep between retries (if atxferdropcall = no)','10',NULL,1,0)";
-$sqls[] = "INSERT IGNORE INTO `featurecodes` VALUES ('extraoptions','atxfercallbackretries','Call back retries','In-Call. Number of times to attempt to send the call back to the transferer.','2',NULL,1,0)";
+$sqls[] = "INSERT IGNORE INTO `featurecodes` (modulename,featurename,description,helptext,defaultcode,customcode,enabled,providedest) VALUES ('extraoptions','transferdigittimeout','Seconds to wait between digits when transferring','In-Call. Number of seconds to wait between digits when transferring a call','3',NULL,1,0)";
+$sqls[] = "INSERT IGNORE INTO `featurecodes` (modulename,featurename,description,helptext,defaultcode,customcode,enabled,providedest) VALUES ('extraoptions','featuredigittimeout','Max time between digits for feature activation','In-Call. Max time (ms) between digits for feature activation (default is 1000 ms)','1000',NULL,1,0)";
+$sqls[] = "INSERT IGNORE INTO `featurecodes` (modulename,featurename,description,helptext,defaultcode,customcode,enabled,providedest) VALUES ('extraoptions','atxfernoanswertimeout','Attended transfer timeout','In-Call. Timeout for answer on attended transfer default is 15 seconds.','15',NULL,1,0)";
+$sqls[] = "INSERT IGNORE INTO `featurecodes` (modulename,featurename,description,helptext,defaultcode,customcode,enabled,providedest) VALUES ('extraoptions','atxferloopdelay','Number of seconds to sleep between retries','In-Call. Number of seconds to sleep between retries (if atxferdropcall = no)','10',NULL,1,0)";
+$sqls[] = "INSERT IGNORE INTO `featurecodes` (modulename,featurename,description,helptext,defaultcode,customcode,enabled,providedest) VALUES ('extraoptions','atxfercallbackretries','Call back retries','In-Call. Number of times to attempt to send the call back to the transferer.','2',NULL,1,0)";
 
 $errors = array();
 foreach ($sqls as $sql) {
